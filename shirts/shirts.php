ab9f27ab59eb8ee30a15ac51f9cb8218bfb8e184
knavarre <knavarre@iupui.edu>
2014-06-02 23:53:38 -0400

    Added pagination

--- shirts/shirts.php
+++ shirts/shirts.php
@@ -1,25 +1,52 @@
 <?php
 require_once("../inc/config.php");
 require_once(ROOT_PATH . 'inc/products.php'); 
-
-$products = get_products_all();
 	
+if (empty($_GET["pg"])) {
+	$current_page = 1;
+} else {
+	$current_page = $_GET["pg"];
+}
+
+$current_page = intval($current_page);
+	
+$total_products = get_products_count();
+$products_per_page = 8;
+$total_pages = ceil($total_products / $products_per_page);
+
+if ($current_page > $total_pages) {
+header("Location: ./?pg=" . $total_pages);
+}
+
+if ($current_page < 1) {
+header("Location: ./");
+}
+
+$start = (($current_page - 1) * $products_per_page) + 1;
+$end = $current_page * $products_per_page;
+if ($end > $total_products) {
+$end = $total_products;
+}
+
+$products = get_products_subset($start,$end);
+
 ?><?php
 $pageTitle = "Mike's Full Catalog of Shirts";
 $section = "shirts";
-include(ROOT_PATH . 'inc/header.php'); 
-?>
+include(ROOT_PATH . 'inc/header.php'); ?>
 
 <div class="section shirts page">
 <div class="wrapper">
 	<h1>Mike&rsquo;s Full Catalog of Shirts</h1>
     
-    	<ul class="products">
-		<?php foreach($products as $product) { 
-		echo get_list_view_html($product);
-		}
-		?>
-		</ul>
+    	<?php include(ROOT_PATH . "inc/partial-list-navigation.html.php"); ?>
 		
+		<ul class="products">
+		<?php foreach($products as $product) {
+			include(ROOT_PATH . "inc/partial-product-list-view.html.php"); }?>
+		</ul>
+
+		<?php include(ROOT_PATH . "inc/partial-list-navigation.html.php"); ?>
+
 </div></div>
 <?php include(ROOT_PATH . "inc/footer.php"); ?>
